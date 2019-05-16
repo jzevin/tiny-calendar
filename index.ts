f93b74e4d7768ef5382159992ffcf6dac23d8a2c
Jeremy Zevin <jeremy.zevin@gmail.com>
2019-05-15 23:41:17 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -52,6 +52,10 @@ class CalendarMonth {
   get year() {
     return this._year;
   }
+
+  get monthOfTheYear(): MonthOfTheYear {
+    return this._monthOfTheYear;
+  }
 }
 
 class Calendar {
@@ -64,41 +68,66 @@ class Calendar {
     year = year || new Date().getFullYear();
     month = month || new Date().getMonth();
     this.year = year;
-    this._month = new CalendarMonth(year, month)
+    this.setMonth(month);
+  }
+
+  get year (): number {
+    return this._year;
   }
 
   set year (year) {
     this._year = year;
+    // month must be changed on year change
   }
 
-  set month (month: MonthOfTheYear) {
-    // this._month = {
-    //   name: MonthOfTheYear[month],
-    //   days: []
-    // };
+  public setMonth(moty: MonthOfTheYear) {
+    this._month = new CalendarMonth(this.year, moty)
   }
 
-  static isDate (d: any): boolean {
+  public getNextMonth(): CalendarMonth {
+    let nextMonth = this._month.monthOfTheYear+1;
+    let year = this._year;
+    if(nextMonth > 11) {
+      nextMonth = MonthOfTheYear.January;
+      year++;
+    } 
+    return new CalendarMonth(year, nextMonth)
+  }
+
+  public getPreviousMonth(): CalendarMonth {
+    let nextMonth = this._month.monthOfTheYear-1;
+    let year = this._year;
+    if(nextMonth < 0) {
+      nextMonth = MonthOfTheYear.December;
+      year--;
+    } 
+    return new CalendarMonth(year, nextMonth)
+  }
+
+  public get monthName (): string {
+    return this._month.name;
+  }
+
+  public static isDate (d: any): boolean {
     return d instanceof Date && !isNaN(d);
   }
 
-  static isIntAndNum(num: any) {
+  public static isIntAndNum(num: any): boolean{
     return Number.isInteger(num) && !Number.isNaN(num);
   }
 
-  static isBetween(num: number, min: number, max: number) {
+  public static isBetween(num: number, min: number, max: number) : boolean{
     return num >= min && num <= max;
   }
 
 }
 
-function extractMonth() {
-  const d = new Date('May 16 2019');
-  log(d, DayOfTheWeek[d.getDay()]);
-}
-
-
-log(new Calendar())
+const cal = new Calendar(1973);
+log(cal.monthName);
+cal.setMonth(MonthOfTheYear.January);
+log(cal.monthName);
+log(cal.getPreviousMonth().year);
+log(cal.monthName);
 // for(const d of new CalendarMonth(MonthOfTheYear.September, 1973).days) {
 //   console.log( d );
 // }
